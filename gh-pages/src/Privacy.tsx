962ac0eb910ea917b5e23e77484914291e74e5e1
Avinash Ranjan <ranjan.avinash@hotmail.com>
2025-01-14 02:48:48 +0530

    feat:link import error fixed

--- gh-pages/src/Privacy.tsx
+++ gh-pages/src/Privacy.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function PrivacyPolicy() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 via-purple-900 to-gray-900">
